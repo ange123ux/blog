a93d439403992be2815e4b815d8b507c9b0fa7be
Ange Uwimana <uuwange@gmail.com>
2020-12-15 11:52:52 +0200

    removed comments

--- app/models.py
+++ app/models.py
@@ -12,15 +12,12 @@ class User (UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),unique = True,nullable = False)
     email = db.Column(db.String(255), unique = True,nullable = False)
-    bio = db.Column(db.String(255),default ='My default Bio')
+    bio = db.Column(db.String(255),default ='Drop your bio here!')
     profile_pic_path = db.Column(db.String(150),default ='default.png')
     hashed_password = db.Column(db.String(255),nullable = False)
     blog = db.relationship('Blog', backref='user', lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
     profile_pic = db.Column(db.String(255))
-    # photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
-
-
 
     @property
     def set_password(self):
